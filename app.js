4ddc6bb383ee893be54d009941fa209478f60577
Jimmy Huang <jimmy@netivism.com.tw>
2013-07-15 17:26:54 -0400

    Add async support for better performance.

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
-// third party
 var http = require('http');
 var url = require('url');
 var request = require('request');
 var cheerio = require('cheerio');
 var fs = require('fs');
+var async = require('async');
 
-// local
 var wiki = require('./lib/wiki');
+var timeline = require('./lib/timeline');
+
+// temporary test for performance
+require('nodetime').profile({
+  accountKey: '137e3c7c701e3322cd82c5867500fd9b07d36608', 
+  appName: 'Node.js Application'
+});
 
 var server = http.createServer(function(req, res){
-  var serverError = function(code, content) {
-    res.writeHead(code, {'Content-Type': 'text/plain'});
-    res.end(content);
-  }
-
   // block error request first
   var path = url.parse(decodeURIComponent(req.url));
 
@@ -21,36 +22,53 @@ var server = http.createServer(function(req, res){
   if(req.method === 'POST'){
     serverError(404, '404 Bad Request');
   }
-  if(path.pathname.match(/^\/wiki\/.+\.json$/g)){
-    var page = path.pathname.replace('/wiki/', '').replace('.json','');
-    var uri = 'http://'+wiki.option.domain+'/'+wiki.option.render+page;
 
-    request(uri, function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        var dom =  cheerio.load(body);
-        var json = wiki.parse(dom, body, page);
-        fs.writeFile('./public/cache/wiki/'+page+'.json', json);
+  // see which path we need to follow
+  // we suppose all the data format is json
+  // var arg0 = path.pathname.match(/^\/[^\/]+/); // not use this yet
+  var key = path.pathname.replace(/^\/[^\/]+\/|\.[^\/]+$/g, '');
+  var ext = path.pathname.lastIndexOf('.') === -1 ? null : path.pathname.split('.').pop();
+
+  if(!ext && key){
+    var source = fs.readFileSync('./page.tpl', 'utf-8').replace();
+    var html = source.replace('example_json.json', '/wiki/'+key+'.json');
+    res.writeHead(200, {"Content-Type": "text/html"});
+    res.end(html);
+  }
+  else
+  if(ext === 'json'){
+    timeline.init();
+    async.parallel({
+      w: function wr(callback){
+        setTimeout(function wrt(){
+          wiki.request(key, callback);
+        }, 100);
+      }
+      /*
+      ,
+      f: function fr(flickr.request){
+        setTimeout(function frt(){
+          flickr.request(path.pathname);
+        }, 300);
+      }
+      */
+    },
+    function endParallel(err, results) {
+      if(err){
+        res.writeHead(501, {'Content-Type': 'text/plain'});
+        res.end('Request problem');
+      }
+      else{
+        var json = timeline.exportjson();
+        fs.writeFile('./public/cache/wiki/'+key+'.json', json);
         res.write(json, 'utf8');
         res.end();
       }
-    })
-    // should be end inside
-  }
-  else
-  if(path.pathname.match(/^\/wiki\/[^.]+$/g)){
-    var page = path.pathname.replace('/', '');
-    var source = fs.readFileSync('./page.tpl', 'utf-8').replace();
-    var html = source.replace('example_json.json', '/'+page+'.json');
-    if(html === false){
-      http.serverError(404, 'Could not found keyword on wikipedia');
-    }
-    else{
-      res.writeHead(200, {"Content-Type": "text/html"});
-      res.end(html);
-    }
+    });
   }
   else{
-    serverError(404, '404 Bad Request');
+    res.writeHead(404, {'Content-Type': 'text/plain'});
+    res.end('404 not found.');
   }
 });
 
