91a19d976350681a426b9eac40c4bcc67e1cc933
Jimmy Huang <jimmy@netivism.com.tw>
2013-07-31 00:49:17 -0400

    First angular try.

--- public/js/index.js
+++ public/js/index.js
@@ -1,19 +1,16 @@
-$(function() {
-  function Arrow_Points() { 
-    var s = $('#container').find('.item');
-
-    $.each(s,function(i,obj) {
-      var posLeft = $(obj).css("left");
-      $(obj).addClass('borderclass');
-      if(posLeft == "0px") {
-        html = "<span class='rightCorner'></span>";
-        $(obj).prepend(html); 
-      } else {
-        html = "<span class='leftCorner'></span>";
-        $(obj).prepend(html);
-      }
-    });
-  }
+function runMasonry() { 
   $('#container').masonry({itemSelector : '.item',});
-  Arrow_Points();
-});
+  var s = $('#container').find('.item');
+
+  $.each(s,function(i,obj) {
+    var posLeft = $(obj).css("left");
+    $(obj).addClass('borderclass');
+    if(posLeft == "0px") {
+      html = "<span class='rightCorner'></span>";
+      $(obj).prepend(html); 
+    } else {
+      html = "<span class='leftCorner'></span>";
+      $(obj).prepend(html);
+    }
+  });
+}
