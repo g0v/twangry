7040688210fdeaeb34374b164386d1738006ad6f
Jimmy Huang <jimmy@netivism.com.tw>
2013-07-31 01:04:28 -0400

    refs #6, add search function by angularjs.

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,29 @@
+function EventCtrl($scope, $http, $templateCache){
+  $scope.method = 'GET';
+  $scope.url = 'http://test.jimmyhub.net/index.json'; 
+  $http({method: $scope.method, url: $scope.url, cache: $templateCache}).
+    success(function(data, status) {
+      $scope.status = status;
+      $scope.events = data;
+    }).
+    error(function(data, status) {
+      $scope.data = data || "Request failed";
+      $scope.status = status;
+  });
+}
+angular.module('index', [])
+.directive('postRepeatMasonry', function() {
+  return function(scope, element, attrs) {
+    if (scope.$last){
+      console.log('last');
+      runMasonry();
+    }
+  };
+});
+
 function runMasonry() { 
   $('#container').masonry({itemSelector : '.item',});
   var s = $('#container').find('.item');
-
   $.each(s,function(i,obj) {
     var posLeft = $(obj).css("left");
     $(obj).addClass('borderclass');
