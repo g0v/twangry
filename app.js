ce96e7dc7412ca1f5bc64b922f5e27115d955a46
Jimmy Huang <jimmy@netivism.com.tw>
2014-03-15 16:04:49 +0800

    Restructure.

--- app.js
+++ app.js
@@ -1,17 +1,13 @@
 var http = require('http');
 var config = require('config');
 var route = require('./lib/route');
-var port = config.port;
 
 var server = http.createServer(function(req, res){
   route.watch(req);
-  route.run(req, res, port);
+  route.run(req, res);
   req.on('error', function (err) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write('404 Not Found\n');
-    res.end();
+    console.log(err);
   });
-});
+}).listen(config.port);
 
-server.listen(port);
-console.log('server is working on port ' + port);
+console.log('server is working on port ' + config.port);
